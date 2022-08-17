9c407ad40443bdd8aa5ec90877d731eaff1416ed
arofre <arofre307@gmail.com>
2022-08-17 15:23:15 +0200

    Added the input for the numbers and the SM to work. Input buttons still wip

--- main.py
+++ main.py
@@ -1,41 +1,57 @@
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import FadeTransition
+from kivy.uix.gridlayout import GridLayout
+from kivy.core.window import Window
+from kivy.uix.floatlayout import FloatLayout
 
+
+# Create the manager
+sm = ScreenManager()
 kivy.require('1.9.0')
+
+Window.size = (360, 760)
 
 class root(BoxLayout):
     def __init__(self):
         super(root, self).__init__()
 
+class MenuScreen(Screen):
     def process(self):
         self.text = self.ids.input.text
+        print(self.text)
+    pass
 
     def submit(self):
         number_list = []
         for i in self.text:
-            number_list.append(i)
-        print(number_list)
-        correct = True
-        for i in number_list:
+            number_list.append(int(i))
+
+class GuessScreen(Screen):
+    def guess(self, number):
+        for i in MenuScreen.number_list:
             if correct == True:
-                guess = input("Input")
-                if guess == i:
+                if number == i:
                     print("Correct, next number")
                     correct = True
                 else:
                     print("Wrong")
                     correct = False
-        if correct == True:
-            print("Every number was correct")
+    pass
 
-
-
+class SettingsScreen(Screen):
+    pass
 
 class Gui(App):
-
     def build(self):
-        return root()
+        sm = ScreenManager(transition=FadeTransition())
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(GuessScreen(name='guess'))
+        sm.add_widget(SettingsScreen(name='settings'))
+        sm.current = 'menu'
+        return sm
 
 gui = Gui()
 gui.run()
