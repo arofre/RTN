b7ec6fc2458c51d14d1ea571c2b5cc038a6afbca
Aron Fredriksson <97045231+arofre@users.noreply.github.com>
2022-08-20 15:30:47 +0200

    Program is now TECHNICALLY functional. Guessing works and takes you to main menu

--- main.py
+++ main.py
@@ -6,39 +6,46 @@ from kivy.uix.screenmanager import FadeTransition
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.uix.floatlayout import FloatLayout
+from kivy.clock import Clock
 
-
-# Create the manager
 sm = ScreenManager()
 kivy.require('1.9.0')
-
 Window.size = (360, 760)
-
 class root(BoxLayout):
     def __init__(self):
         super(root, self).__init__()
 
 class MenuScreen(Screen):
+    number_list = []
     def process(self):
         self.text = self.ids.input.text
         print(self.text)
     pass
 
     def submit(self):
+        global number_list
+        global initial_length
         number_list = []
         for i in self.text:
             number_list.append(int(i))
+        self.ids.input.text = ''
+        initial_length = len(number_list)
 
 class GuessScreen(Screen):
     def guess(self, number):
-        for i in MenuScreen.number_list:
-            if correct == True:
-                if number == i:
-                    print("Correct, next number")
-                    correct = True
-                else:
-                    print("Wrong")
-                    correct = False
+        if number == number_list[0]:
+            number_list.pop(0)
+            if number_list == []:
+                self.ids.guess_text.text = 'All numbers are correct'
+                Clock.schedule_once(self.change_screen_menu, 3)
+
+        elif number != number_list[0]:
+            self.ids.guess_text.text = f'Incorrect, you guessed {initial_length - len(number_list)} / {initial_length}'
+            Clock.schedule_once(self.change_screen_menu, 3)
+
+    def change_screen_menu(self, obj):
+        self.manager.current = 'menu'
+        self.ids.guess_text.text = 'Guess the number?'
     pass
 
 class SettingsScreen(Screen):
@@ -53,5 +60,6 @@ class Gui(App):
         sm.current = 'menu'
         return sm
 
-gui = Gui()
-gui.run()
+if __name__ == '__main__' :
+    gui = Gui()
+    gui.run()
